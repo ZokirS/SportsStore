c6b29ad30a976a245a83f4b9df68f8cd305037a4
ZokirS <zokir5857@gmail.com>
2023-07-13 12:18:04 +0500

    add product CRUD operations in Blazor pages

--- SportsStore/Models/EFStoreRepository.cs
+++ SportsStore/Models/EFStoreRepository.cs
@@ -8,5 +8,22 @@
             _context = context;
         }
         public IQueryable<Product> Products => _context.Products;
+
+        public void CreateProduct(Product product)
+        {
+            _context.Add(product);
+            _context.SaveChanges();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            _context.Remove(product);
+            _context.SaveChanges();
+        }
+
+        public void SaveProduct(Product product)
+        {
+            _context.SaveChanges();
+        }
     }
 }
